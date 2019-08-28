f13d2fe01a27076ca2dcd234fec8d24a8d08531f
DESKTOP-KF2J20I\paolo <paolo.innocenti@estrobit.com>
2019-08-28 09:30:59 +0200

    fix buildpackage

--- src/PH.PicoCrypt2/BuildPackage.bat
+++ src/PH.PicoCrypt2/BuildPackage.bat
@@ -5,6 +5,5 @@ rm -Rf "P:\Dev\Gitlab\PH.PicoCrypt2\BuildPackages"
 
 dotnet pack "P:\Dev\Gitlab\PH.PicoCrypt2\src\PH.PicoCrypt2\PH.PicoCrypt2\PH.PicoCrypt2.csproj" -c Release --include-symbols --include-source  -o "P:\Dev\Gitlab\PH.PicoCrypt2\BuildPackages"
 
-nuget push  "P:\Dev\Gitlab\PH.PicoCrypt2\BuildPackages\PH.PicoCrypt2.*.nupkg" -Source "https://api.nuget.org/v3/index.json"
 
 echo "done"
